ddc93395066e320553bb8ec6b68dfeefef083246
Edvard All <edvard.all@gmail.com>
2026-01-07 20:46:54 +0100

    clean slate

--- polybar/hack/scripts/monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1
--- polybar/scripts/scroll_spotify_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# see man zscroll for documentation of the following parameters
-zscroll -l 30 \
-        --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
-        --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" &
-
-wait
